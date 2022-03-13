ca4f4686dfab5cfae5e5b75558e7d876aff252fc
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-03-13 20:24:41 +0300

    Remove Nuget UniTask

--- ProjectAlpha/Assets/Code/MyScript.cs
+++ ProjectAlpha/Assets/Code/MyScript.cs
@@ -1,10 +1,11 @@
-using Cysharp.Threading.Tasks;
+// using Cysharp.Threading.Tasks;
 
 namespace Code
 {
     public static class MyScript
     {
         public static int Field = 3;
-        public static string UnitTaskName = nameof(UniTask);
+        // public static string UnitTaskName = nameof(UniTask);
+        public static string UnitTaskName = "UniTask";
     }
 }
