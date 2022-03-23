c9deaffe6e5aaf78dba56725c21d55f2f29de974
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-03-23 16:41:36 +0300

    Add Zenject NSubstitute

---
+++ ProjectAlpha/Assets/Tests/UnitTests/MyUnitTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using Zenject;
+
+public class MyUnitTest : ZenjectUnitTestFixture
+{
+    [Test]
+    void Test1() { }
+}
