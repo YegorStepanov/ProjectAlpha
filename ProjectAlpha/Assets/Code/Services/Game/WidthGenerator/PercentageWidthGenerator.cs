4ac432a93c9eeadad005660c229f95561c8dddbb
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-05-01 15:49:48 +0300

    Add todo comment

--- ProjectAlpha/Assets/Code/Services/Game/WidthGenerator/PercentageWidthGenerator.cs
+++ ProjectAlpha/Assets/Code/Services/Game/WidthGenerator/PercentageWidthGenerator.cs
@@ -4,7 +4,7 @@ using Random = UnityEngine.Random;
 namespace Code.Services;
 
 [CreateAssetMenu(menuName = "SO/Percentage Width Generator")]
-public sealed class PercentageWidthGenerator : WidthGenerator
+public sealed class PercentageWidthGenerator : WidthGenerator //todo: split to 2 classes
 {
     // [SerializeField, MinMaxSlider(0, 1), LabelText("Range of the platform")]
     // private Vector2 platformViewportRange = new(0.1f, 0.5f);
