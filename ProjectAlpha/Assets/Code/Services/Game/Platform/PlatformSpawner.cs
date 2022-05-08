18567b8c0c35f8d5bd73723c4a5a3847104d1a98
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-05-08 18:48:54 +0300

    Refactor PlatformSpawner for pooling

--- ProjectAlpha/Assets/Code/Services/Game/Platform/PlatformSpawner.cs
+++ ProjectAlpha/Assets/Code/Services/Game/Platform/PlatformSpawner.cs
@@ -1,32 +1,35 @@
-﻿using UnityEngine;
-using VContainer;
-using VContainer.Unity;
+﻿using System.Collections.Generic;
+using Code.VContainer;
+using UnityEngine;
 
 namespace Code.Services;
 
 public sealed class PlatformSpawner
 {
-    private readonly LifetimeScope _scope;
-    private readonly IObjectResolver _resolver;
-    private readonly PlatformController _platformPrefab;
+    private readonly MonoBehaviourPool<PlatformController> _pool;
     private readonly CameraController _cameraController;
 
-    public PlatformSpawner(
-        LifetimeScope scope,
-        IObjectResolver resolver,
-        PlatformController platformPrefab,
-        CameraController cameraController)
+    private readonly List<PlatformController> _platforms;
+
+    private int _platformIndex;
+
+    public PlatformSpawner(MonoBehaviourPool<PlatformController> pool, CameraController cameraController)
     {
-        _scope = scope;
-        _resolver = resolver;
-        _platformPrefab = platformPrefab;
+        _pool = pool;
         _cameraController = cameraController;
+        _platforms = new List<PlatformController>(_pool.Count);
+        _platformIndex = 0;
     }
 
     public IPlatformController CreatePlatform(Vector2 position, float width, Relative relative)
     {
-        PlatformController platform = _resolver.Instantiate(_platformPrefab, _scope.transform);
-        platform.transform.SetParent(null);
+        if (_pool.TrySpawn(out PlatformController platform))
+            _platforms.Add(platform);
+        else
+        {
+            platform = _platforms[_platformIndex];
+            _platformIndex = (_platformIndex + 1) % _platforms.Count;
+        }
 
         float height = _cameraController.Borders.TransformPointY(position.y, Relative.Bottom);
 
