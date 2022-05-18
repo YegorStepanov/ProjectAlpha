1ad663309df6028cae6605bbfa3979ef3e50e51b
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-05-18 22:11:33 +0300

    Add ClearCache

---
+++ ProjectAlpha/Assets/Editor/EditorMenus.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace Code.Editor;
+
+public static class EditorMenus
+{
+    [MenuItem("Tools/Clear addressables cache", false, 50)]
+    public static void ClearAddressablesCache()
+    {
+        UnityEngine.Caching.ClearCache();
+    }
+}
