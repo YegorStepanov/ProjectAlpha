bd836ddaf619b7b0c9aebfb284ef949c8a4fb962
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-05-20 01:23:31 +0300

    Add ConstWidthGenerator

---
+++ ProjectAlpha/Assets/Code/Services/Game/WidthGenerator/ConstWidthGeneratorData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Code.Services;
+
+[CreateAssetMenu(menuName = "SO/Const Width Generator")]
+public sealed class ConstWidthGeneratorData : WidthGeneratorData
+{
+    [Min(0f)]
+    public float Width = 2f;
+
+    public override IWidthGenerator Create() => new ConstWidthGenerator(this);
+}
