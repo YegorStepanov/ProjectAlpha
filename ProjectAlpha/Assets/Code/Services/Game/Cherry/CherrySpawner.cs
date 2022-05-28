59bf1b702f402fc99d7d2d8c7aabcabdbb43f68a
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-05-29 00:54:29 +0300

    Spawn cherry on the left side of the platform

--- ProjectAlpha/Assets/Code/Services/Game/Cherry/CherrySpawner.cs
+++ ProjectAlpha/Assets/Code/Services/Game/Cherry/CherrySpawner.cs
@@ -7,26 +7,20 @@ namespace Code.Services;
 public sealed class CherrySpawner
 {
     private readonly IAsyncPool<CherryController> _pool;
-    private readonly CameraController _cameraController;
 
-    public CherrySpawner(IAsyncPool<CherryController> pool, CameraController cameraController)
-    {
+    public CherrySpawner(IAsyncPool<CherryController> pool) =>
         _pool = pool;
-        _cameraController = cameraController;
-    }
 
     public async UniTask<ICherryController> CreateCherryAsync(IPlatformController currentPlatform,
         IPlatformController nextPlatform)
     {
         CherryController cherry = await _pool.SpawnAsync();
 
-        float distanceWhichCameraWillMove = currentPlatform.Borders.Left - _cameraController.Borders.Left;
-
         Vector2 position = new(
-            _cameraController.Borders.Right + distanceWhichCameraWillMove,
+            nextPlatform.Borders.Left,
             nextPlatform.Borders.Top);
 
-        cherry.TeleportTo(position, Relative.LeftTop);
+        cherry.TeleportTo(position, Relative.RightTop);
         return cherry;
     }
 }
