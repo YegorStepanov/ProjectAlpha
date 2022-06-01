369261229a5606cb1d9075841d3b1e39b25b1c22
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-06-01 12:52:51 +0300

    Add properties to the Borders

--- ProjectAlpha/Assets/Code/Common/Borders.cs
+++ ProjectAlpha/Assets/Code/Common/Borders.cs
@@ -4,7 +4,24 @@ namespace Code;
 
 public readonly record struct Borders(float Top, float Bottom, float Left, float Right)
 {
-    public Vector2 Center => new((Left + Right) / 2f, (Top + Bottom) / 2f);
     public float Width => Right - Left;
     public float Height => Top - Bottom;
+
+    public float HalfWidth => Width / 2f;
+    public float HalfHeight => Height / 2f;
+    
+    public Vector2 LeftTop => new(Left, Top);
+    public Vector2 LeftCenter => new(Left, CenterY);
+    public Vector2 LeftBottom => new(Left, Bottom);
+    
+    public Vector2 RightTop => new(Right, Top);
+    public Vector2 RightCenter => new(Right, CenterY);
+    public Vector2 RightBottom => new(Right, Bottom);
+    
+    public Vector2 CenterTop => new(CenterX, Top);
+    public Vector2 Center => new(CenterX, CenterY);
+    public Vector2 CenterBottom => new(CenterX, Bottom);
+    
+    private float CenterX => (Left + Right) / 2f;   
+    private float CenterY => (Top + Bottom) / 2f;
 }
