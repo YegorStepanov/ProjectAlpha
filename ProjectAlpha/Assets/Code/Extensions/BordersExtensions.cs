6e65ed58fc990850edfebb0ed4fb44e5c48fb8e9
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-06-07 04:24:51 +0300

    Add extension methods to Borders

--- ProjectAlpha/Assets/Code/Extensions/BordersExtensions.cs
+++ ProjectAlpha/Assets/Code/Extensions/BordersExtensions.cs
@@ -8,6 +8,12 @@ public static class BordersExtensions
     public static Vector3 GetRelativePoint(this Borders borders, Vector3 point, Relative relative) =>
         borders.GetRelativePoint((Vector2)point, relative).WithZ(point.z);
 
+    public static float GetRelativePointX(this Borders borders, float pointX, Relative relative) =>
+        borders.GetRelativePoint(new Vector2(pointX, 0), relative).x;
+
+    public static float GetRelativePointY(this Borders borders, float pointY, Relative relative) =>
+        borders.GetRelativePoint(new Vector2(0, pointY), relative).y;
+
     public static Vector2 GetRelativePoint(this Borders borders, Vector2 point, Relative relative) =>
         relative switch
         {
@@ -22,7 +28,7 @@ public static class BordersExtensions
             Relative.RightBot => point - borders.RightBot,
             _ => throw new InvalidEnumArgumentException(nameof(relative), (int)relative, typeof(Relative))
         };
-    
+
     public static bool Intersect(this Borders borders, Borders other)
     {
         return !(other.Left > borders.Right || other.Right < borders.Left ||
@@ -40,4 +46,16 @@ public static class BordersExtensions
         return point.x >= borders.Left && point.x <= borders.Right &&
                point.y >= borders.Bot && point.y <= borders.Top;
     }
+
+    public static Borders ShiftX(this Borders borders, float offsetX) =>
+        borders.Shift(new Vector2(offsetX, 0));
+
+    public static Borders ShiftY(this Borders borders, float offsetY) =>
+        borders.Shift(new Vector2(0, offsetY));
+
+    public static Borders Shift(this Borders borders, Vector2 offset) =>
+        new(borders.Top + offset.y,
+            borders.Bot + offset.y,
+            borders.Left + offset.x,
+            borders.Right + offset.x);
 }
