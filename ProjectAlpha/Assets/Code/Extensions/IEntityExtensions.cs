c9f4e2b6283a276d42c593e163ac7fb1510c6515
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-06-08 12:32:21 +0300

    Replace interface extension methods to generic

--- ProjectAlpha/Assets/Code/Extensions/IEntityExtensions.cs
+++ ProjectAlpha/Assets/Code/Extensions/IEntityExtensions.cs
@@ -6,12 +6,12 @@ namespace Code;
 // ReSharper disable once InconsistentNaming
 public static class IEntityExtensions
 {
-    public static bool Intersect(this IEntity entity, IEntity other) =>
+    public static bool Intersect<T1, T2>(this T1 entity, T2 other) where T1 : IEntity where T2 : IEntity =>
         entity.Borders.Intersect(other.Borders);
 
-    public static bool Inside(this IEntity entity, IEntity other) =>
+    public static bool Inside<T1, T2>(this T1 entity, T2 other) where T1 : IEntity where T2 : IEntity =>
         entity.Borders.Inside(other.Borders);
 
-    public static bool Contains(this IEntity entity, Vector2 point) =>
+    public static bool Contains<T1>(this T1 entity, Vector2 point) where T1 : IEntity =>
         entity.Borders.Inside(point);
 }
