f5032cc6206365d22f9a6b0009c2227b52924bea
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-06-08 12:46:24 +0300

    Refactor data

--- ProjectAlpha/Assets/Code/Services/Game/UI/GameData.cs
+++ ProjectAlpha/Assets/Code/Services/Game/UI/GameData.cs
@@ -1,11 +1,16 @@
-﻿namespace Code.Services.Game.UI;
+﻿using UnityEngine;
+using VContainer.Unity;
 
-public sealed class GameData
+namespace Code.Services.Game.UI;
+
+public sealed class GameData : IInitializable, IStartable
 {
+    private const string CherryCountKey = "CherryCount";
+
     private readonly Camera _camera;
     private readonly Settings _settings;
-    
-    public int Score { get; private set; } = -1;
+
+    public int Score { get; private set; }
     public int CherryCount { get; private set; }
     public float GameHeight { get; private set; }
 
@@ -14,20 +19,35 @@ public sealed class GameData
         _camera = camera;
         _settings = settings;
     }
-    
+
+    void IInitializable.Initialize()
+    {
+        Score = -1;
+        CherryCount = LoadCherryCount();
+    }
+
+    void IStartable.Start()
+    {
+        //it should be an camera event
+        GameHeight = _camera.ViewportToWorldPositionY(_settings.ViewportMenuHeight);
+    }
+
     public void IncreaseScore() =>
         Score++;
 
-    public void IncreaseCherryCount() =>
+    public void IncreaseCherryCount()
+    {
         CherryCount++;
+        SaveCherryCount(CherryCount);
+    }
 
-    public void SetGameHeight() =>
-        GameHeight = _camera.ViewportToWorldPositionY(_settings.ViewportGameHeight);
+    private static int LoadCherryCount() =>
+        PlayerPrefs.GetInt(CherryCountKey, 0);
 
-    //todo
-    public void SetMenuHeight() =>
-        GameHeight = _camera.ViewportToWorldPositionY(_settings.ViewportMenuHeight);
-    
+    private static void SaveCherryCount(int value) =>
+        PlayerPrefs.SetInt(CherryCountKey, value);
+
+
     [System.Serializable]
     public class Settings
     {
