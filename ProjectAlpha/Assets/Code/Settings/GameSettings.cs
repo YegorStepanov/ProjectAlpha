4986ab3fafd8752b1e8d13f7e3c87d989861c574
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-08-08 05:23:23 +0300

    Reorder settings

--- ProjectAlpha/Assets/Code/Settings/GameSettings.cs
+++ ProjectAlpha/Assets/Code/Settings/GameSettings.cs
@@ -13,25 +13,25 @@ public sealed class GameSettings
     [SerializeField, LabelText("DelayBeforeHeroMovement (s)")]
     private float _delayBeforeHeroMovement = 0.2f;
     [SerializeField, LabelText("DelayBeforeFalling (s)")]
-    [Space]
     private float _delayBeforeFalling = 0.1f;
     [SerializeField, LabelText("DelayBeforeFalling (s)")]
     private float _delayBeforeRotatingStickDown = 0.1f;
-
-    public float ViewportMenuPlatformPositionX = 0.5f;
-    public float MenuPlatformWidth = 2f;
-    [MinValue(0), MaxValue(1)]
-    public float ViewportHeight = 0.2f;
-    [Space]
-    [MinValue(-1), MaxValue(1)]
-    public float ViewportCameraMovementYOnGameStart = 0.1f; //bug when value < 0
-
-    [Range(0, 1)]
-    public float CherryChance = 0.1f;
 
     public int DelayAfterHeroMovementToPlatform => (int)(_delayBeforeNextRound * 1000);
     public int DelayBeforeEndGameInEndGame => (int)(_delayBeforeEndGame * 1000);
     public int DelayBeforeHeroMovement => (int)(_delayBeforeHeroMovement * 1000);
     public int DelayBeforeFallingInEndGame => (int)(_delayBeforeFalling * 1000);
     public int DelayBeforeRotatingStickDownInEndGame => (int)(_delayBeforeRotatingStickDown * 1000);
+
+    [Space]
+    public float ViewportMenuPlatformPositionX = 0.5f;
+    public float MenuPlatformWidth = 2f;
+    [MinValue(0), MaxValue(1)]
+    public float ViewportHeight = 0.2f;
+    [MinValue(-1), MaxValue(1)]
+    public float ViewportCameraMovementYOnGameStart = 0.1f; //bug when value < 0
+
+    [Space]
+    [Range(0, 1)]
+    public float CherryChance = 0.1f;
 }
