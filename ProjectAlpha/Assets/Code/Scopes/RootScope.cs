4495ec5842ba11f06bf7f7c5883078eb0de9f875
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-08-10 15:39:05 +0300

    Reorder scope

--- ProjectAlpha/Assets/Code/Scopes/RootScope.cs
+++ ProjectAlpha/Assets/Code/Scopes/RootScope.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Code.AddressableAssets;
 using Code.Extensions;
 using Code.Services;
@@ -27,7 +28,7 @@ public sealed class RootScope : Scope
 
     protected override async UniTask PreloadAsync(IAddressablesLoader loader)
     {
-        (_camera, _settingsFacade, _loadingScreen, _) = await (
+        (_camera, _settingsFacade, _loadingScreen, var a) = await (
             loader.InstantiateAsync(Address.Infrastructure.CameraController, inject: false),
             loader.LoadAssetAsync(Address.Infrastructure.Settings),
             loader.InstantiateAsync(Address.UI.TransitionLoadingScreen, inject: false),
@@ -47,30 +48,21 @@ public sealed class RootScope : Scope
 
     protected override void ConfigureServices(IContainerBuilder builder)
     {
-        builder.Register<HeroSelector>(Lifetime.Singleton);
-        builder.RegisterInstance<IReadOnlyList<Address<Hero>>>(new[]
-        {
-            Address.Entity.Hero.Hero1,
-            Address.Entity.Hero.Hero2,
-            Address.Entity.Hero.Hero3,
-            Address.Entity.Hero.Hero4,
-        });
-
         RegisterSettings(builder);
         RegisterCamera(builder);
+        RegisterProgress(builder);
+        RegisterInfrastructure(builder);
+        RegisterDevelopment(builder);
 
-        RegisterInput(builder);
-        builder.Register<IRandomizer, Randomizer>(Lifetime.Singleton);
+        RegisterAddressableLoaders(builder);
 
-        RegisterSceneLoader(builder);
-        RegisterLoaders(builder);
+        RegisterLoadingScreen(builder);
+        RegisterHeroSelector(builder);
+
         RegisterAds(builder);
         RegisterIAP(builder);
 
-        RegisterProgress(builder);
         RegisterMessagePipe(builder);
-        RegisterDevelopment(builder);
-        RegisterLoadingScreen(builder);
 
         builder.RegisterEntryPoint<RootEntryPoint>();
     }
@@ -83,9 +75,16 @@ public sealed class RootScope : Scope
     private void RegisterCamera(IContainerBuilder builder)
     {
         builder.RegisterComponent(_camera);
+        builder.RegisterComponent(_camera.Background).WithParameter((IReadOnlyList<Address<Texture2D>>)new[]
+        {
+            Address.Background.Background1,
+            Address.Background.Background2,
+            Address.Background.Background3,
+            Address.Background.Background4,
+            Address.Background.Background5,
+        });
+        //builder.Inject(_camera.Background);
 
-        builder.RegisterInstance(_camera.Background);
-        builder.Inject(_camera.Background);
         builder.RegisterInstance<IReadOnlyList<Address<Texture2D>>>(
             new List<Address<Texture2D>>
             {
@@ -97,17 +96,30 @@ public sealed class RootScope : Scope
             });
     }
 
-    private static void RegisterInput(IContainerBuilder builder)
+    private static void RegisterProgress(IContainerBuilder builder)
     {
-        builder.Register<IInputManager, InputManager>(Lifetime.Singleton);
+        builder.Register<IPersistentProgress, PersistentProgress>(Lifetime.Singleton);
+        builder.Register<ISessionProgress, SessionProgress>(Lifetime.Singleton);
+        builder.Register<IProgress, Progress>(Lifetime.Singleton);
     }
 
-    private static void RegisterSceneLoader(IContainerBuilder builder)
+    private static void RegisterInfrastructure(IContainerBuilder builder)
     {
+        builder.Register<IInputManager, InputManager>(Lifetime.Singleton);
+        builder.Register<IRandomizer, Randomizer>(Lifetime.Singleton);
         builder.Register<ISceneLoader, SceneLoader>(Lifetime.Singleton);
     }
 
-    private static void RegisterLoaders(IContainerBuilder builder)
+    private void RegisterDevelopment(IContainerBuilder builder)
+    {
+        if (PlatformInfo.IsDevelopment)
+        {
+            builder.RegisterNonLazy<DevelopmentRootPanel>(Lifetime.Singleton);
+            builder.RegisterComponentAndInjectGameObject(_developmentPanel);
+        }
+    }
+
+    private static void RegisterAddressableLoaders(IContainerBuilder builder)
     {
         builder.Register<ICreator, Creator>(Lifetime.Scoped);
         builder.Register<IAddressablesCache, AddressablesCache>(Lifetime.Scoped);
@@ -115,9 +127,25 @@ public sealed class RootScope : Scope
         builder.Register<IGlobalAddressablesLoader, GlobalAddressablesLoader>(Lifetime.Scoped);
     }
 
+    private void RegisterLoadingScreen(IContainerBuilder builder)
+    {
+        builder.RegisterInstance(_loadingScreen);
+    }
+
+    private static void RegisterHeroSelector(IContainerBuilder builder)
+    {
+        builder.Register<HeroSelector>(Lifetime.Singleton).WithParameter((IReadOnlyList<Address<Hero>>)new[]
+        {
+            Address.Entity.Hero.Hero1,
+            Address.Entity.Hero.Hero2,
+            Address.Entity.Hero.Hero3,
+            Address.Entity.Hero.Hero4,
+        });
+    }
+
     private static void RegisterAds(IContainerBuilder builder)
     {
-        builder.Register<BannerAd>(Lifetime.Transient).AsImplementedInterfaces().AsSelf();
+        builder.Register<BannerAd>(Lifetime.Transient).As<IDisposable>().AsSelf();
 
         builder.Register<InterstitialAd>(Lifetime.Transient);
         builder.Register<RewardedAd>(Lifetime.Transient);
@@ -133,32 +161,11 @@ public sealed class RootScope : Scope
         builder.Register<IIAPManager, IAPManager>(Lifetime.Singleton);
     }
 
-    private static void RegisterProgress(IContainerBuilder builder)
-    {
-        builder.Register<IPersistentProgress, PersistentProgress>(Lifetime.Singleton);
-        builder.Register<ISessionProgress, SessionProgress>(Lifetime.Singleton);
-        builder.Register<IProgress, Progress>(Lifetime.Singleton);
-    }
-
     private static void RegisterMessagePipe(IContainerBuilder builder)
     {
         MessagePipeOptions options = builder.RegisterMessagePipe();
         //Setup GlobalMessagePipe to enable diagnostics window and global function
         builder.RegisterBuildCallback(c => GlobalMessagePipe.SetProvider(c.AsServiceProvider()));
         builder.RegisterMessageBroker<Event.GameStart>(options);
-    }
-
-    private void RegisterDevelopment(IContainerBuilder builder)
-    {
-        if (PlatformInfo.IsDevelopment)
-        {
-            builder.RegisterNonLazy<DevelopmentRootPanel>(Lifetime.Singleton);
-            builder.RegisterComponentAndInjectGameObject(_developmentPanel);
-        }
-    }
-
-    private void RegisterLoadingScreen(IContainerBuilder builder)
-    {
-        builder.RegisterInstance(_loadingScreen);
     }
 }
