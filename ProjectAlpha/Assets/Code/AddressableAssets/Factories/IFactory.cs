e9b1c76fdf3d30bb58dec3a465706a73ccd169b9
Yegor Stepanov <yegor.stepanov@outlook.com>
2023-05-02 16:18:48 +0300

    Fix comment

--- ProjectAlpha/Assets/Code/AddressableAssets/Factories/IFactory.cs
+++ ProjectAlpha/Assets/Code/AddressableAssets/Factories/IFactory.cs
@@ -1,6 +1,6 @@
 ﻿namespace Code.AddressableAssets
 {
-//check performance of covariance modifier
+    // Check performance of covariance modifier
     public interface IFactory<out TValue>
     {
         TValue Create();
