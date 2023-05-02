a58c004cb3c822c004fd10637b0b948dd5b35ac8
Yegor Stepanov <yegor.stepanov@outlook.com>
2023-05-02 04:58:53 +0300

    Fix changing hero from deadlock

--- ProjectAlpha/Assets/Code/Services/Navigators/MenuSceneNavigator.cs
+++ ProjectAlpha/Assets/Code/Services/Navigators/MenuSceneNavigator.cs
@@ -1,4 +1,5 @@
-﻿using Code.Services.Infrastructure;
+﻿using Code.Common;
+using Code.Services.Infrastructure;
 using Code.UI;
 using Cysharp.Threading.Tasks;
 
@@ -25,13 +26,15 @@ namespace Code.Services.Navigators
             {
                 await _loadingScreen.FadeInAsync();
 
-                await _sceneLoader.UnloadAsync<GameScene>();
-                await _sceneLoader.LoadAsync<GameScene>();
+                await using (var _ = _gameResourcesLoadedEvent.Wait().ToUniTaskDisposable())
+                {
+                    // TODO load 2 new scenes first in parallel and then unload the first two scenes in parallel also
+                    await _sceneLoader.UnloadAsync<GameScene>();
+                    await _sceneLoader.LoadAsync<GameScene>();
 
-                await _sceneLoader.UnloadAsync<MenuScene>();
-                await _sceneLoader.LoadAsync<MenuScene>();
-
-                await _gameResourcesLoadedEvent.Wait();
+                    await _sceneLoader.UnloadAsync<MenuScene>();
+                    await _sceneLoader.LoadAsync<MenuScene>();
+                }
                 await _loadingScreen.FadeOutAsync();
             }
         }
